ba0e88b47b14ab24776c827a2cbef57f97b238fa
Jingxuan Feng <henryfeng24@pm.me>
2021-08-15 22:15:14 +1000

    Finished preliminary tests for DepositSlotTimedTestSimpleRepeatS3843790.java

--- src/test/java/au/edu/rmit/ct/DepositSlotTimedTestSimpleRepeatS3843790.java
+++ src/test/java/au/edu/rmit/ct/DepositSlotTimedTestSimpleRepeatS3843790.java
@@ -9,13 +9,10 @@
 
 package au.edu.rmit.ct;
 
+import static java.time.Duration.ofMillis;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 /*
  * Please replace S3214321 of class name with your own student number
@@ -23,25 +20,46 @@ import org.junit.jupiter.api.Test;
 
 class DepositSlotTimedTestSimpleRepeatS3843790 {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	//Test naming convention used: MethodName_ExpectedBehavior_StateUnderTest
+	@Nested
+	class DepositSlotTimedConstructorTest{
+		@Test
+		@DisplayName("1. Test Default Constructor")
+		void DepositSlotTimed_SuccessfullyCreated_DefaultConstructor() {
+			DepositSlotTimed TestDepositSlotTimed = new DepositSlotTimed("Test");
+
+			assertFalse(TestDepositSlotTimed.isOpenForDeposit(), "Expect False");
+		}
+
+		@Test
+		@DisplayName("2. Test Constructor")
+		void DepositSlotTimed_SuccessfullyCreated_Constructor() throws Exception {
+			DepositSlotTimed TestDepositSlotTimed = new DepositSlotTimed("Test2", 3000);
+
+			assertFalse(TestDepositSlotTimed.isOpenForDeposit(), "Expect False");
+		}
+
+		@Test
+		@DisplayName("3. Test CustomWaitTime is too high")
+		void DepositSlotTimed_ThrowsException_CustomWaitTimeIsTooHigh() throws Exception {
+			assertThrows(Exception.class, () -> new DepositSlotTimed("Test2", 5001), "Expect Exception");
+		}
+
+		@Test
+		@DisplayName("4. Test CustomWaitTime is too low")
+		void DepositSlotTimed_ThrowsException_CustomWaitTimeIsNegative() throws Exception {
+			assertThrows(Exception.class, () -> new DepositSlotTimed("Test2", -100), "Expect Exception");
+		}
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	@RepeatedTest(10)
+	@DisplayName("5. Attempting to locate error margin")
+	void checkTimer_HalfOfTestsWillFail_AttemptingToLocateErrorMargin() throws Exception {
+		DepositSlotTimed TestDepositSlotTimed = new DepositSlotTimed("Test2", 4999);
+		assertTimeout(ofMillis(4999), () -> {
+			TestDepositSlotTimed.checkTimer();
+		});
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
