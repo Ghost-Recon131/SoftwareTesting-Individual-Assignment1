c555f4399670243e89b99fa11b3038c7d102ac1e
Jingxuan Feng <henryfeng24@pm.me>
2021-08-19 16:34:36 +1000

    Progress on 2nd set of tests

--- src/test/java/au/edu/rmit/ct/BankDatabaseNewTestSortingS3843790.java
+++ src/test/java/au/edu/rmit/ct/BankDatabaseNewTestSortingS3843790.java
@@ -31,6 +31,12 @@ class BankDatabaseNewTestSortingS3843790 {
 	CustomerAccount TestAccount10;
 	CustomerAccount TestAccount11;
 	CustomerAccount TestAccount12;
+	CustomerAccount TestAccount13;
+	CustomerAccount TestAccount14;
+	CustomerAccount TestAccount15;
+	CustomerAccount TestAccount16;
+	CustomerAccount TestAccount17;
+	CustomerAccount TestAccount18;
 
 	void SetUpTestDataSet1() throws Exception{
 		TestAccount1 = new CustomerAccount(100, 6296, 2000, 4000, "Galleu", "Rem", 205486);
@@ -47,6 +53,15 @@ class BankDatabaseNewTestSortingS3843790 {
 		TestAccount12 = new CustomerAccount(10, 5678, 1000, 2000, "Miyazono", "Kaori", 123789);
 	}
 
+	void SetUpTestDataSet2() throws Exception{
+		TestAccount13 = new CustomerAccount(1101, 7056, 1500, 3000, "Gura", "Gwar", 244650); // Same BSB and Balance as TestAccount 10
+		TestAccount14 = new CustomerAccount(1111, 6602, 100000, 100000, "Ogiwara", "Issa", 732817); // Same family name & BSB as TestAccount2
+		TestAccount15 = new CustomerAccount(10000, 7917, 6500, 13000, "Tachibana", "Mitsuha", 728824); // Same family name as TestAccount3
+		TestAccount16 = new CustomerAccount(10001, 4557, 5500, 11000, "Tokisaki", "Shido", 151915);; // Same Balance, BSB and family name as TestAccount 8
+		TestAccount17 = new CustomerAccount(10010, 1772, 3500, 7000, "Tempest", "Rimuru", 335368); // Same Balance as TestAccount5
+		TestAccount18 = new CustomerAccount(10100, 9176, 7000, 14000, "Bloodfallen", "Shalltear", 974680); // Same BSB as TestAccount 12
+	}
+
 	@AfterEach
 	void tearDown() throws Exception {
 		TestAccount1 = null;
@@ -61,6 +76,12 @@ class BankDatabaseNewTestSortingS3843790 {
 		TestAccount10 = null;
 		TestAccount11 = null;
 		TestAccount12 = null;
+		TestAccount13 = null;
+		TestAccount14 = null;
+		TestAccount15 = null;
+		TestAccount16 = null;
+		TestAccount17 = null;
+		TestAccount18 = null;
 	}
 
 	//Test naming convention used: MethodName_ExpectedBehavior_StateUnderTest
@@ -171,6 +192,51 @@ class BankDatabaseNewTestSortingS3843790 {
 		ManuallySortedByName.add(TestAccount7);
 
 		assertEquals(ManuallySortedByName, BankDatabaseTest.export(), "Expect equals");
+	}
+
+	// Repeating to ensure the behaviour of being ordered after the 'original' remains consistent
+	@RepeatedTest(3)
+	@DisplayName("4. Matching manually sorted Arraylist to arraylist sorted by sortByTotalBalance with dataset 2")
+	void sortByTotalBalance_CorrectlySorts_SomeAccountsWithBalance() throws Exception {
+		BankDatabaseNew BankDatabaseTest = new BankDatabaseNew();
+		SetUpTestDataSet1();
+		SetUpTestDataSet2();
+		BankDatabaseTest.add(TestAccount1); // add the test accounts to BankDatabase
+		BankDatabaseTest.add(TestAccount2);
+		BankDatabaseTest.add(TestAccount3);
+		BankDatabaseTest.add(TestAccount4);
+		BankDatabaseTest.add(TestAccount5);
+		BankDatabaseTest.add(TestAccount6);
+		BankDatabaseTest.add(TestAccount7);
+		BankDatabaseTest.add(TestAccount8);
+		BankDatabaseTest.add(TestAccount9);
+		BankDatabaseTest.add(TestAccount10);
+		BankDatabaseTest.add(TestAccount11);
+		BankDatabaseTest.add(TestAccount12);
+		BankDatabaseTest.add(TestAccount13); // same as 10
+		BankDatabaseTest.add(TestAccount16); // same as 8
+		BankDatabaseTest.add(TestAccount17); // same as 5
+		BankDatabaseTest.sortByTotalBalance();
+
+		//create arraylist where we manually sort the accounts then we will match against the arraylist sorted by the program
+		ArrayList<CustomerAccount> ManuallySortedByTotalBalance = new ArrayList<>();
+		ManuallySortedByTotalBalance.add(TestAccount4);
+		ManuallySortedByTotalBalance.add(TestAccount12);
+		ManuallySortedByTotalBalance.add(TestAccount10);
+		ManuallySortedByTotalBalance.add(TestAccount13); // same as 10
+		ManuallySortedByTotalBalance.add(TestAccount1);
+		ManuallySortedByTotalBalance.add(TestAccount7);
+		ManuallySortedByTotalBalance.add(TestAccount6);
+		ManuallySortedByTotalBalance.add(TestAccount5);
+		ManuallySortedByTotalBalance.add(TestAccount17); // same as 5
+		ManuallySortedByTotalBalance.add(TestAccount11);
+		ManuallySortedByTotalBalance.add(TestAccount9);
+		ManuallySortedByTotalBalance.add(TestAccount3);
+		ManuallySortedByTotalBalance.add(TestAccount8);
+		ManuallySortedByTotalBalance.add(TestAccount16); // same as 8
+		ManuallySortedByTotalBalance.add(TestAccount2);
+
+		assertEquals(ManuallySortedByTotalBalance, BankDatabaseTest.export(), "Expect equals, accounts with same details will come after the 'original' since they are added later");
 	}
 
 }
